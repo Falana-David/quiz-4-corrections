6ba5704bf53aef82531438ccd5dfaf71aaa0707e
Falana-David <falanadavid@gmail.com>
2019-01-21 18:51:31 -0500

    Create partial.java

---
+++ partial.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class partial
+{
+public String partial(String original, String letter, int count) {
+
+	Scanner s = new Scanner(System.in);
+	System.out.println("What Is String Original?");
+    String original1 = s.nextLine();
+    System.out.println("What Letter?");
+    String letter1 = s.next();
+    System.out.println("What Count?");
+    int count1 = s.nextInt();
+	if (original1 == null)
+	{
+		while (original1 != null) {
+			String original11 = line.substringpartial(original11, letter1, count1);
+		}	
+	}
+	else if (original1 != null)
+	{
+		return null;
+	}
+	
+}
+}
